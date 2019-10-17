03529c773a0e49637bef0e1d8d9b9f7c30fc7c52
PieterGeers <48602601+PieterGeers@users.noreply.github.com>
2019-10-17 11:47:07 +0300

    added map w/ marker

--- Scripts/locationscript.js
+++ Scripts/locationscript.js
@@ -1,4 +1,6 @@
 var loc = document.getElementById('myloc');
+var mapdiv = document.getElementById('map');
+var map;
 
 function myLocation() {
 	if (navigator.geolocation) {
@@ -10,9 +12,45 @@ function myLocation() {
 }
 
 function showPosition(position) {
-	loc.innerHTML = "Longitude: " + round(position.coords.longitude,5) + "<br>Latitude: " + round(position.coords.latitude, 5) + "<br>";
+	var mapLat = round(position.coords.latitude, 5);
+	var mapLng = round(position.coords.longitude,5);
+	var mapDefaultZoom = 10;
+	mapdiv.classList.remove("invis");
+	initialize_map(mapLng, mapLat, mapDefaultZoom);
+	add_map_point(mapLat, mapLng);
+	loc.innerHTML = "Latitude: " + mapLat + " - Longitude: " + mapLng;
 }
 
 function round(x, n) {
     return Math.round(x * Math.pow(10, n)) / Math.pow(10, n)
+}
+
+
+
+function initialize_map(maplong, maplat, defzoom) {
+	map = new ol.Map({
+		target: "map", layers: [new ol.layer.Tile({
+			source: new ol.source.OSM({url: "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				})
+			})
+		], view: new ol.View({
+			center: ol.proj.fromLonLat([maplong, maplat]), zoom: defzoom
+			})
+		});
+	}
+
+function add_map_point(lat, lng) {
+	var vectorLayer = new ol.layer.Vector({
+		source:new ol.source.Vector({
+			features: [new ol.Feature({
+				geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
+				})]
+			}), style: new ol.style.Style({
+				image: new ol.style.Icon({
+					anchor: [0.5, 0.5], anchorXUnits: "fraction", anchorYUnits: "fraction",
+						src: "https://upload.wikimedia.org/wikipedia/commons/e/ec/RedDot.svg"
+					})
+				})
+			});
+	map.addLayer(vectorLayer);
 }
